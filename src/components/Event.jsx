0be4dbdd8fd7d076195471b4ec9292ecf8a4e9e6
Basit46 <Hassanbasit1990@gmail.com>
2023-09-25 15:45:30 +0100

    Made changes to Event container

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -31,8 +31,8 @@ const Event = ({ event }) => {
   };
 
   return (
-    <div className="event w-[90%] vsm:w-[300px] sm:w-[308px] h-[414px] relative flex flex-col justify-between rounded-[18.28px] border border-zinc-300 overflow-hidden px-[12px] pt-[14.85px] pb-[9.12px]">
-      <div className="max-h-[16%]">
+    <div className="event w-[90%] vsm:w-[300px] sm:w-[308px] h-fit vsm:h-[414px] relative flex flex-col justify-between gap-[20px] vsm:gap-0 rounded-[18.28px] border border-zinc-300 overflow-hidden px-[12px] pt-[14.85px] pb-[9.12px]">
+      <div className="h-fit vsm:max-h-[16%]">
         {event.name.length > 20 ? (
           <h1 className="text-center text-white text-[14px] font-bold">
             {event.name}
@@ -45,15 +45,15 @@ const Event = ({ event }) => {
       </div>
 
       <div className="relative w-full h-[118px] bg-opacity-40 bg-zinc-300 rounded-lg overflow-hidden flex justify-center">
-        <img src={event.imgurl} alt="event img" className=" object-cover" />
+        <img src={event?.imgurl} alt="event img" className="object-cover" />
       </div>
-      <div className="bg-black bg-opacity-30 h-[105px] max-h-[105px] rounded-[7px] px-[9px] py-[5px]">
+      <div className="bg-black bg-opacity-30 min-h-[105px] h-fit vsm:h-[105px] vsm:max-h-[105px] rounded-[7px] px-[9px] py-[5px]">
         {event.desc.length > 200 ? (
-          <p className=" text-white text-[0.7rem] leading-[1.2] font-bold">
+          <p className="text-white text-[0.7rem] leading-[1.2] font-bold">
             {event.desc}
           </p>
         ) : (
-          <p className=" text-white text-[0.9rem] leading-[1.2] font-bold">
+          <p className="text-white text-[0.9rem] leading-[1.2] font-bold">
             {event.desc}
           </p>
         )}
